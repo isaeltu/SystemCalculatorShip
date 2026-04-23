9a262b961ccbccf6ae7b59bcb911599fd194d0e5
Isael Capellan Valdez <137923069+isaeltu@users.noreply.github.com>
2026-04-23 02:59:17 +0000

    Global ef

---
+++ SystemCalculatorShip.Infrastructure/Persistence/Contexts/ApplicationDbContextFactory.cs
@@ -0,0 +1,22 @@
+namespace SystemCalculatorShip.Infrastructure.Persistence.Contexts;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+/// <summary>
+/// Design-time factory used by EF Core tools to create ApplicationDbContext for migrations.
+/// </summary>
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var connectionString =
+            Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")
+            ?? "Server=(localdb)\\MSSQLLocalDB;Database=SystemCalculatorShipDb;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
